d7067cb3f018a9f13e2204a78f259cbfd558e095
gowtham <gowtham.chinnahambi@treselle.com>
2016-10-25 20:26:48 +0530

    Adjusted for naming conventions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Weloome to node frontend container');
+  res.send('Welome to front-end container');
 });
 
 app.listen(3004, function () {
